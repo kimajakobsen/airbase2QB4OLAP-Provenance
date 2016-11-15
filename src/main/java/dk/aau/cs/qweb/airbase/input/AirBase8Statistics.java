eb99aee16621ed65b94457c9136f8bbde9d02908
kimajakobsen <kimajakobsen@gmail.com>
2016-11-15 20:27:44 +0100

    these are not use any more, but I need the lists defined in them when the cubeStructure is implemented.

--- src/main/java/dk/aau/cs/qweb/airbase/input/AirBase8Statistics.java
+++ src/main/java/dk/aau/cs/qweb/airbase/input/AirBase8Statistics.java
@@ -1,16 +1,45 @@
 package dk.aau.cs.qweb.airbase.input;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import dk.aau.cs.qweb.airbase.types.Tuple;
 
-public class AirBase8Statistics implements FileStructure {
+public class AirBase8Statistics{
 
+	private static List<String> header = new ArrayList<String>() {
+		private static final long serialVersionUID = 2L;
 
-	@Override
+		{			
+			add("station_european_code");
+			add("component_code");
+			add("component_name");
+			add("component_caption");
+			add("measurement_unit");
+			add("measurement_european_group_code");
+			add("statistics_period");
+			add("statistics_year");
+			add("statistics_average_group");
+			add("statistic_shortname");
+			add("statistic_name");
+			add("statistic_value");
+			add("statistics_percentage_valid");
+			add("statistics_number_valid");
+			add("statistics_calculated");
+		}
+	};
+
+	public AirBase8Statistics(String file) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public Iterator<Tuple> iterator() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	public static List<String> getHeader() {
+		return header;
+	}
 }
