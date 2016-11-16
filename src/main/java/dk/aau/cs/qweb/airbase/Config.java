3077fda68e275d81f70088740f8690bfccce3167
kimajakobsen <kimajakobsen@gmail.com>
2016-11-16 10:50:09 +0100

    start implementing the Triple container

--- src/main/java/dk/aau/cs/qweb/airbase/Config.java
+++ src/main/java/dk/aau/cs/qweb/airbase/Config.java
@@ -2,4 +2,9 @@ package dk.aau.cs.qweb.airbase;
 
 public class Config {
 
+	public static String getNamespace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/main/java/dk/aau/cs/qweb/airbase/types/TripleContainer.java
+++ src/main/java/dk/aau/cs/qweb/airbase/types/TripleContainer.java
@@ -3,29 +3,35 @@ package dk.aau.cs.qweb.airbase.types;
 import java.util.ArrayList;
 import java.util.List;
 
+import dk.aau.cs.qweb.airbase.Config;
+
 public class TripleContainer {
 
 	private Tuple tuple;
 
 	public TripleContainer(Tuple tuple) {
 		this.tuple = tuple;
-		List<Quad> triples = new ArrayList<Quad>();
-		String subject = createSubject();
+		List<Quad> informationTriples = new ArrayList<Quad>();
+		List<Quad> metadataTriples = new ArrayList<Quad>();
+		List<Quad> provenanceTriples = new ArrayList<Quad>();
+		
 		int index = 0;
 		
 		for (String predicateString : this.tuple.getHeader()) {
-			String predicate = CubeStructure.getPredicate(predicateString);
 			
-			if (!predicate.equals(null)) {
+			if (CubeStructure.isPredicatePartOfCube(predicateString)) {
+				String predicate = CubeStructure.getPredicate(predicateString);
+				String subject = createSubject(predicate);
 				String object = tuple.getData().get(index);
+				
 				Quad quad =  new Quad(subject,predicate,object);
 				String graphLabel = getGraphLabel(quad);
 				quad.setGraphLabel(graphLabel);
-				triples.add(quad);
+				
+				informationTriples.add(quad);
 			}
 			index++;
 		}
-		
 	}
 
 	private String getGraphLabel(Quad quad) {
@@ -33,9 +39,21 @@ public class TripleContainer {
 		return null;
 	}
 
-	private String createSubject() {
-		// TODO Auto-generated method stub
-		return null;
+	private String createSubject(String predicate) {
+		List<Integer> indexes = CubeStructure.getPrimaryKeyIndexes(tuple.getHeader(),predicate);
+		String subject = Config.getNamespace();
+		for (Integer index : indexes) {
+			subject += tuple.getData().get(index)+"_";
+		}
+		subject = replacelastUnderscoreWithSlash(subject);
+		return subject;
 	}
 
+	private String replacelastUnderscoreWithSlash(String str) {
+	    if (str != null && str.length() > 0 && str.charAt(str.length()-1)=='_') {
+	      str = str.substring(0, str.length()-1);
+	      str += "/";
+	    }
+	    return str;
+	}
 }
