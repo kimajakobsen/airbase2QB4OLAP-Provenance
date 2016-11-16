3d8417d4194023973bd305bc818f71389ad3d196
kimajakobsen <kimajakobsen@gmail.com>
2016-11-16 09:06:12 +0100

    make getters and setter

--- src/main/java/dk/aau/cs/qweb/airbase/types/Quad.java
+++ src/main/java/dk/aau/cs/qweb/airbase/types/Quad.java
@@ -25,4 +25,33 @@ public class Quad {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getPredicate() {
+		return predicate;
+	}
+
+	public void setPredicate(String predicate) {
+		this.predicate = predicate;
+	}
+
+	public String getObject() {
+		return object;
+	}
+
+	public void setObject(String object) {
+		this.object = object;
+	}
+
+	public String getGraphLabel() {
+		return graphLabel;
+	}
+	
 }
