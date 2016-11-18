bc6a8caa41a656341de22e3f157452758166076c
kimajakobsen <kimajakobsen@gmail.com>
2016-11-18 13:36:10 +0100

    adding new functionallity. Now a value can be retrived based on column name.

--- src/main/java/dk/aau/cs/qweb/airbase/types/Tuple.java
+++ src/main/java/dk/aau/cs/qweb/airbase/types/Tuple.java
@@ -1,11 +1,12 @@
 package dk.aau.cs.qweb.airbase.types;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Tuple {
 
-	private List<String> data;
-	private List<String> header;
+	private List<String> data = new ArrayList<String>();
+	private List<String> header = new ArrayList<String>();
 
 	public Tuple(List<String> data, List<String> header) {
 		this.data = data;
@@ -18,6 +19,31 @@ public class Tuple {
 	
 	public List<String> getHeader() {
 		return header;
+	}
+	
+	public String getValue(String header) {
+		int index = 0;
+		String result = "";
+		for (String string : this.header) {
+			if (header.equals(string)) {
+				result = data.get(index);
+			}
+			index++;
+		}
+		return result;
+	}
+	
+	@Override
+	public String toString() {
+		String string = "";
+		int index = 0;
+		for (String column : header) {
+			string += column+": ";
+			string += data.get(index)+", ";
+			index++;
+		}
+		
+		return string;
 	}
 
 }
