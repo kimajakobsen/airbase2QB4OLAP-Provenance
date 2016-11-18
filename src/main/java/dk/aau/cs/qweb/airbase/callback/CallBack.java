b30a9cf9ea9b88add8c57dff6c3f6bf4ed17e272
kimajakobsen <kimajakobsen@gmail.com>
2016-11-18 13:38:39 +0100

    made a callback class, the purpose of this is to be invoked when the data need to be transformed, this is not done yet.

---
+++ src/main/java/dk/aau/cs/qweb/airbase/callback/CallBack.java
@@ -0,0 +1,5 @@
+package dk.aau.cs.qweb.airbase.callback;
+
+public interface CallBack {
+	String callBackMethod(String string); 
+}
---
+++ src/main/java/dk/aau/cs/qweb/airbase/callback/YesNo2TrueFalse.java
@@ -0,0 +1,12 @@
+package dk.aau.cs.qweb.airbase.callback;
+
+public class YesNo2TrueFalse implements CallBack {
+
+
+	@Override
+	public String callBackMethod(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
