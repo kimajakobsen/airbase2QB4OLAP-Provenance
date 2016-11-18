ccb8eb017db2dcaf0b761c1681ee02553814102c
kimajakobsen <kimajakobsen@gmail.com>
2016-11-18 13:37:27 +0100

    add cube concepts classes

---
+++ src/main/java/dk/aau/cs/qweb/airbase/Qb4OLAP/HierarchyStep.java
@@ -0,0 +1,34 @@
+package dk.aau.cs.qweb.airbase.Qb4OLAP;
+
+public class HierarchyStep {
+
+	public HierarchyStep(String subject) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setChildLevel(String object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setHierarchy(String object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setParentLevel(String object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setCardinality(String object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setRollup(String object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
