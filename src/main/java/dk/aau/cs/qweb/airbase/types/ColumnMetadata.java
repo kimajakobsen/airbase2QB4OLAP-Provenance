ed5e15780eb03605cd67eee2ecee878011607529
kimajakobsen <kimajakobsen@gmail.com>
2016-11-18 13:40:04 +0100

    added a class that is used to contain information about a column from the csv file.

---
+++ src/main/java/dk/aau/cs/qweb/airbase/types/ColumnMetadata.java
@@ -0,0 +1,63 @@
+package dk.aau.cs.qweb.airbase.types;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import dk.aau.cs.qweb.airbase.callback.CallBack;
+
+public class ColumnMetadata {
+
+	private String name = "";
+	private boolean isPartOfCube = false;
+	private Map<String,Boolean> levels = new HashMap<String,Boolean>();
+	private CallBack callback = new CallBackDefault();
+
+	class CallBackDefault implements CallBack {
+		@Override
+		public String callBackMethod(String string) {
+			return string;
+		}
+	}
+	
+	public ColumnMetadata() {
+		this.isPartOfCube = false;
+	}
+	
+	public ColumnMetadata(String name, Map<String,Boolean> levels) {
+		this.name = name;
+		this.isPartOfCube = true;
+		this.levels.putAll(levels);;
+	}
+	
+	public ColumnMetadata(Map<String,Boolean> levels) {
+		this.levels.putAll(levels);;
+	}
+	
+	public ColumnMetadata(String name, Map<String,Boolean> levels, CallBack callback) {
+		this.name = name;
+		this.isPartOfCube = true;
+		this.levels.putAll(levels);
+		this.callback = callback;
+	}
+	
+	public ColumnMetadata(Map<String,Boolean> levels, CallBack callback) {
+		this.levels.putAll(levels);
+		this.callback = callback;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public boolean isPartOfCube() {
+		return isPartOfCube;
+	}
+	
+	public CallBack getCallBackFunction() {
+		return callback;
+	}
+	
+	public Map<String,Boolean> getLevels() {
+		return levels;
+	}
+}
