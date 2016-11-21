3c130dd5feb71b82ba3f4f0c9b6b24f8fd0d1583
kimajakobsen <kimajakobsen@gmail.com>
2016-11-21 17:49:46 +0100

    add path to cube structure

--- src/main/java/dk/aau/cs/qweb/airbase/Config.java
+++ src/main/java/dk/aau/cs/qweb/airbase/Config.java
@@ -2,7 +2,7 @@ package dk.aau.cs.qweb.airbase;
 
 public class Config {
 
-	private static String cubeStructurePath;
+	private static String cubeStructurePath = "src/test/resources/Ontology/Airbase.ttl";
 	private static String namespace = "http://qweb.cs.aau.dk/airbase/";
 
 	public static String getNamespace() {
@@ -10,14 +10,16 @@ public class Config {
 	}
 
 	public static String getCubeStructureFilePath() {
-		// TODO Auto-generated method stub
-		return null;
+		return cubeStructurePath;
 	}
 
 	public static void setCubeStructurePath(String cubeStructurePath) {
 		Config.cubeStructurePath = cubeStructurePath;
+	}
+
+	public static String getMetadataGraphLabel() {
 		// TODO Auto-generated method stub
-		
+		return null;
 	}
 
 }
