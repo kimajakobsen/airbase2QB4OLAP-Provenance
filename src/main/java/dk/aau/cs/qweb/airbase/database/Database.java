e77118d6232183696f2cd094b31c690baf40239f
kimajakobsen <kimajakobsen@gmail.com>
2016-11-23 11:48:09 +0100

    update mocked functions

--- src/main/java/dk/aau/cs/qweb/airbase/database/Database.java
+++ src/main/java/dk/aau/cs/qweb/airbase/database/Database.java
@@ -1,10 +1,10 @@
 package dk.aau.cs.qweb.airbase.database;
 
-import dk.aau.cs.qweb.airbase.types.Tuple;
+import dk.aau.cs.qweb.airbase.types.TripleContainer;
 
 public class Database {
 
-	public void writeToDisk(Tuple tuple) {
+	public void writeToDisk(TripleContainer triples) {
 		// TODO Auto-generated method stub
 		
 	}
