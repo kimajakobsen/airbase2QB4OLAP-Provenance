1258ef659c5eb87884b152806f850fd3fe80e7b1
kimajakobsen <kimajakobsen@gmail.com>
2016-11-24 17:24:15 +0100

    made the provO vocabulary

---
+++ src/main/java/dk/aau/cs/qweb/airbase/vocabulary/PROVvocabulary.java
@@ -0,0 +1,23 @@
+package dk.aau.cs.qweb.airbase.vocabulary;
+
+public class PROVvocabulary {
+	//Namespaces
+	public static String provNS = "http://www.w3.org/ns/prov#";
+	
+	//Prov types
+	public static String Entity = provNS+"Entity";
+	public static String Activity = provNS+"Activity";
+	public static String Agent = provNS+"Agent";
+	public static String Bundle = provNS+"Bundle";
+	public static String SoftwareAgent = provNS+"SoftwareAgent";
+	
+	//prov properties
+	public static String wasAttributedTo = provNS+"wasAttributedTo";
+	public static String used = provNS+"used";
+	public static String generated = provNS+"generated";
+	public static String startedAtTime = provNS+"startedAtTime";
+	public static String endedAtTime = provNS+"endedAtTime";
+	public static String wasGeneratedBy = provNS+"wasGeneratedBy";
+	public static String generatedAtTime = provNS+"generatedAtTime";
+	public static String atLocation = provNS+"atLocation";
+}
