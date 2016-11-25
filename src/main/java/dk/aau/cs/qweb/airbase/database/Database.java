5b4f833ed7b0c5ac93413baf0d1919b1e6a9482d
kimajakobsen <kimajakobsen@gmail.com>
2016-11-25 09:41:00 +0100

    added a small test dataset to test resources

--- src/main/java/dk/aau/cs/qweb/airbase/database/Database.java
+++ src/main/java/dk/aau/cs/qweb/airbase/database/Database.java
@@ -1,11 +1,16 @@
 package dk.aau.cs.qweb.airbase.database;
 
+import java.util.Set;
+
+import dk.aau.cs.qweb.airbase.types.Quad;
 import dk.aau.cs.qweb.airbase.types.TripleContainer;
 
 public class Database {
 
 	public void writeToDisk(TripleContainer triples) {
-		// TODO Auto-generated method stub
+		Set<Quad> informationTriples = triples.getInformationTriples();
+		Set<Quad> metadataTriples = triples.getMetadataTriples();
+		Set<Quad> provenanceTriples = triples.getProvenanceTriples();
 		
 	}
 
