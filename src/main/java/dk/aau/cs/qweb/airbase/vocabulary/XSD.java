2ed79606c0938b93d7da82502cd0e1a8f68c4b3c
kimajakobsen <kimajakobsen@gmail.com>
2016-11-27 13:51:53 +0100

    added a XSD type class

---
+++ src/main/java/dk/aau/cs/qweb/airbase/vocabulary/XSD.java
@@ -0,0 +1,12 @@
+package dk.aau.cs.qweb.airbase.vocabulary;
+
+public class XSD {
+	//Namespaces
+	public static String xsdNS = "http://www.w3.org/ns/prov#";
+	
+	public static String booleanType = "^^"+xsdNS+"boolean";
+	public static String stringType = "^^"+xsdNS+"string";
+	public static String integerType = "^^"+xsdNS+"integer";
+	public static String decimalType = "^^"+xsdNS+"decimal";
+	public static String dateType = "^^"+xsdNS+"date";
+}
