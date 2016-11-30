c5d70be349aa6cad84957fc91f082f1532127743
kimajakobsen <kimajakobsen@gmail.com>
2016-11-30 18:16:35 +0100

    clean up

--- src/main/java/dk/aau/cs/qweb/airbase/provenance/provo/ProvenanceIdentifierEntity.java
+++ src/main/java/dk/aau/cs/qweb/airbase/provenance/provo/ProvenanceIdentifierEntity.java
@@ -3,9 +3,6 @@ package dk.aau.cs.qweb.airbase.provenance.provo;
 public class ProvenanceIdentifierEntity extends Entity {
 
 	public ProvenanceIdentifierEntity() {
-		
 		super("provenanceIdentifer");
-		// TODO Auto-generated constructor stub
 	}
-
 }
